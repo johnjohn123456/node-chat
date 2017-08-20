ee8bca7c6dbb6c6f94daeaba6a816acfb2e891b7
John Af <Johnaf123456@gmail.com>
2017-08-20 15:15:46 +0300

    communication

--- public/js/index.js
+++ public/js/index.js
@@ -2,16 +2,13 @@
         socket.on('connect',()=>{
             console.log("Connection");
             
-           socket.emit('createMessage',{
-               from:'sakis@gmail.com',
-               text:"ela re magkoura"
-           });
+       
         })
         
         socket.on('disconnect',()=>{
             console.log("disconnected from server");
         })
 
-       socket.on('newMessage',function (message) {
-           console.log('message' ,message);
-       });
+     socket.on('newMessage',function(message){
+         console.log("newMessage" ,message);
+     })
--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const http = require('http');
 var app=express();
 const port =process.env.PORT || 3000;
 var publicPath=path.join(__dirname + '/../public');
-;
+
 
 var server = http.createServer(app);
 var io=socketIO(server);
@@ -17,13 +17,15 @@ io.on('connection',(socket) =>{
     socket.on('disconnect',() =>{
     console.log("client disconnect");
 })
-  socket.emit('newMessage',{
-      from:'xaradara@gmail.com',
-      text:'Mourla i omada mourla'
-  })
+  
     
    socket.on('createMessage', (message)=>{
              console.log('createMessage',message);
+       io.emit('newMessage', {
+          from:message.from,
+        text:message.text,
+           createAt:new Date().getTime()
+       });
              });
 })
 
