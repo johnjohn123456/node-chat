060106ee96a95a027bdf1813ee33678f6a762714
John Af <Johnaf123456@gmail.com>
2017-08-21 17:51:59 +0300

    finish comn

--- public/js/index.js
+++ public/js/index.js
@@ -11,4 +11,20 @@
 
      socket.on('newMessage',function(message){
          console.log("newMessage" ,message);
-     })
+     });
+     socket.emit('createMessage',{
+         from :"Sakis",
+         text:"Sindethika"
+     },function (data){
+        console.log('got it' ,data) ;
+     });
+
+    jQuery('#message-form').on('submit',function (e){
+        e.preventDefault();
+        socket.emit('createMessage',{
+            from:"User",
+            text:jQuery('[name=message]').val()
+        },function(){
+            
+        });
+    });
--- server/server.js
+++ server/server.js
@@ -22,13 +22,14 @@ io.on('connection',(socket) =>{
   
     
     socket.broadcast.emit('newMessage', generateMessage('Admin',"New User Connected"));
-   socket.on('createMessage', (message)=>{
+   socket.on('createMessage', (message,callback)=>{
              console.log('createMessage',message);
        io.emit('newMessage', generateMessage(message.from ,message.text));
 //       socket.broadcast.emit('newMessage',{
 //          from:message.from,
 //          text:message.text 
 //       });
+       callback('From server');
              });
 })
 
